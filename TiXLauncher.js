d72dadaaaee1bacb9c62735f0091c6d439ef54e5
RyuaNerin <github@ryuanerin.kr>
2016-03-05 08:47:11 +0900

    Update TiXLauncher.js
    규격변경

--- TiXLauncher.js
+++ TiXLauncher.js
@@ -1,17 +1,24 @@
-// ¾ÆÁî·¹¾Æ TiX ·±Ã³
+// ì•„ì¦ˆë ˆì•„ TiX ëŸ°ì²˜
 // modified by @SasarinoMARi
 // Last Update: 2016-03-02
 // see more info : http://usagination.com
 
-// Ctrl + Shift + C : È­¸é Ä¸Ã³ ÈÄ Æ®À­
-// Ctrl + Shift + V : È­¸é Ä¸Ã³ ÈÄ ¼±ÅÃµÈ Æ®À­¿¡ ¸à¼Ç
+// Ctrl + Shift + C : í™”ë©´ ìº¡ì²˜ í›„ íŠ¸ìœ—
+// Ctrl + Shift + V : í™”ë©´ ìº¡ì²˜ í›„ ì„ íƒëœ íŠ¸ìœ—ì— ë©˜ì…˜
+
+Array.prototype.contains = function(element) {
+	for (var i = 0; i < this.length; i++)
+		if (this[i].toLowerCase() == element.toLowerCase())
+			return true;
+	return false;
+}
 
 FileSystem.privateStore.write('location.dat', System.applicationPath.replace(/[^(.)^(\\)]+(.)exe/, ''), 3);
 function run(exe, arg) {
     var path = FileSystem.privateStore.read('location.dat') + exe;
     System.launchApplication(path, arg, 1);
 }
-System.addContextMenuHandler('Quicx ½ÇÇà', 0, function (id) {
+System.addContextMenuHandler('Quicx ì‹¤í–‰', 0, function (id) {
     run('TiX/TiX.exe', '');
 });
 System.addKeyBindingHandler('C'.charCodeAt(0), 3, function (id) {
@@ -19,6 +26,30 @@ System.addKeyBindingHandler('C'.charCodeAt(0), 3, function (id) {
 });
 System.addKeyBindingHandler('V'.charCodeAt(0), 3, function (id) {
     if (id == undefined) return;
-    var username = TwitterService.status.get(id).user.screen_name;
-    run('TiX/TiX.exe', 'Stasis ' + username + ' ' + id);
+    
+	var status = TwitterService.status.get(id);
+	if (!status) return;
+
+	var status_users = [];
+	TwitterService.status.getUsers(id, status_users);
+
+	var me = TwitterService.currentUser.screen_name.toLowerCase();
+	var new_users = new Array();
+	
+	if (status.user.screen_name.toLowerCase() != me)
+		new_users.push(status.user.screen_name);
+	
+	for (var i = 0; i < status_users.length; i++)
+		if (status_users[i].toLowerCase() != me)
+			if (!new_users.contains(status_users[i]))
+				new_users.push(status_users[i]);
+
+    var str = '';
+	if (new_users.length > 0)
+		str = '@' +  new_users.join(' @') + ' ';
+	else
+		str = '';
+	TextArea.in_reply_to_status_id = status_id;
+    
+    run('TiX/TiX.exe', 'Stasis "' + str + '" "' + id + '"');
 });
